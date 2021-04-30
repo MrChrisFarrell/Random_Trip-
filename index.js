bcdc76ed64b95460be576c718671950d833e42cd
unknown <mrchrisfarrell@yahoo.com>
2021-04-30 14:52:55 -0400

    added tripGenerator function to make the intitial random pick. function asks for confirmation returning a string to be used for changing selections.

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ let restaurants = ["Italian", "Seafood", "American", "Mexican", "Korean", "Japan
 let transportation = ["Car", "Moped", "Public", "Bike", "SUV", "Truck", "Taxi"];
 let entertainment = ["Concert", "Hike", "Casino", "Relaxing massage", "Run with the bulls"];
 
-function randomDest(places){
-    let numberOfPlaces = places.length;
-    let random = Math.floor(Math.random() * numberOfPlaces);
-    let destPick = places[random];
-    return destPick;
+function randomPicker(array){
+    let numberOfArray = array.length;
+    let random = Math.floor(Math.random() * numberOfArray);
+    let pick = array[random];
+
+    return pick;
 }
-let attempt = randomDest(destinations);
-console.log(attempt);
+function tripGenerator(destArray, restArray, transArray, entArray){
+    let trip = [];
+    trip[0] = randomPicker(destArray);
+    trip[1] = randomPicker(restArray);
+    trip[2] = randomPicker(transArray);
+    trip[3] = randomPicker(entArray);
+    let choose = prompt("You are going to " + trip[0] + ", where you will eat " + trip[1] + ". You will be travelling by " + trip[2] + " to enjoy a " + trip[3] + "! Is this good? Type \"yes\" or \"no\".");
+    return choose;
+}
+let choice = tripGenerator(destinations, restaurants, transportation, entertainment);
+console.log(choice);
